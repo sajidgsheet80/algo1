c6c19b19c67dae5c2d1d4c2e76e25cb0a28f4c94
sajidgsheet80 <sajidgsheet80@gmail.com>
2025-09-21 23:39:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -254,5 +254,10 @@ def index():
     rows = fetch_option_chain_data()
     return render_template_string(HTML_TEMPLATE, columns=columns, rows=rows)
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
